9a449d92068d9f88265e3d54443877c6a5c9362e
Dr Maxim Orlovsky <orlovsky@cyphernet.org>
2022-09-29 13:14:47 +0200

    Parametrizing multiplexed frames by payload

--- src/frame/mux.rs
+++ src/frame/mux.rs
@@ -4,29 +4,31 @@ use super::FramedStream;
 use crate::frame::Frame;
 
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
-pub struct MultiplexedFrame {
+pub struct Multiplexed<Payload: Frame> {
     pub channel: u16,
-    pub payload: Box<[u8]>,
+    pub payload: Payload,
 }
 
 #[derive(Debug)]
-pub struct InvalidMultiplexedFrame;
+pub enum MultiplexedError<Payload: Frame> {
+    NoChannel,
+    PayloadParse(Payload::Error),
+}
 
-impl Frame for MultiplexedFrame {
-    type Error = InvalidMultiplexedFrame;
+impl<Payload: Frame> Frame for Multiplexed<Payload> {
+    type Error = MultiplexedError<Payload>;
 
     fn parse(payload: &[u8]) -> Result<Self, Self::Error> {
         if payload.len() < 2 {
-            return Err(InvalidMultiplexedFrame);
+            return Err(MultiplexedError::NoChannel);
         }
         let mut channel = [0u8; 2];
         channel.copy_from_slice(&payload[..2]);
         let channel = u16::from_le_bytes(channel);
-        Ok(Self {
-            channel,
-            payload: Box::from(&payload[2..]),
-        })
+        let payload = Payload::parse(payload).map_err(MultiplexedError::PayloadParse)?;
+        Ok(Self { channel, payload })
     }
 }
 
-pub type Multiplexed64KbFrames<R, W, D, E> = FramedStream<R, W, D, E, MultiplexedFrame, 2u8>;
+pub type Multiplexed64KbFrames<R, W, D, E, Payload> =
+    FramedStream<R, W, D, E, Multiplexed<Payload>, 2u8>;
