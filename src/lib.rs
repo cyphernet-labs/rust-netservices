0d89dc85511427cc53ce1d9b277551165eee72a3
Dr. Maxim Orlovsky <orlovsky@pandoraprime.ch>
2022-10-07 11:52:11 +0200

    Add streampipes library doc comment description

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! A library used for processing streams with filters organized into pipes.
+//! Filters may perform tasks like multiplexing/demultiplexing, encrypting/
+//! decrypting, encoding/decoding stream data, and conversions of the stream
+//! into packets.
+
 pub mod channel;
 pub mod frame;
 pub mod transcode;
