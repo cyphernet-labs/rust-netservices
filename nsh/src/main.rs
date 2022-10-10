98f1a6629f0e15d182a2df05bda4ce5168b220f8
Dr. Maxim Orlovsky <orlovsky@pandoraprime.ch>
2022-10-10 09:08:11 +0200

    nsh: clap integration

--- nsh/src/main.rs
+++ nsh/src/main.rs
@@ -1,3 +1,27 @@
+#[macro_use]
+extern crate clap;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Clone, Debug)]
+#[derive(Parser)]
+#[command(author, version, about)]
+struct Args {
+    #[command(subcommand)]
+    command: Command
+}
+
+#[derive(Clone, Eq, PartialEq, Debug)]
+#[derive(Subcommand)]
+enum Command {
+    /// does testing things
+    Test {
+        /// lists test values
+        #[arg(short, long)]
+        list: bool,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
 }
