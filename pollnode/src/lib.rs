8a14452488bf7de0b2dcf18d4ade3d704849076c
Dr Maxim Orlovsky <orlovsky@cyphernet.org>
2022-10-10 09:52:50 +0200

    Introducing nsh command-line tool

--- pollnode/src/lib.rs
+++ pollnode/src/lib.rs
@@ -1,14 +1,11 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct Runtime<H: nakamoto_net::Protocol> {
+    handler: H
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl<H: nakamoto_net::Protocol> Runtime<H> {
+    pub fn run(&self) -> ! {
+        loop {
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        }
     }
 }
