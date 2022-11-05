ae98cc89c39f770cc71d2cdf27ffa5271e324726
Dr. Maxim Orlovsky <orlovsky@pandoraprime.ch>
2022-11-05 23:16:46 +0100

    TCP-related actors via composition

--- re-actor/src/actors/stdtcp.rs
+++ re-actor/src/actors/stdtcp.rs
@@ -1,6 +1,7 @@
-use std::io::Write;
-use std::net::{SocketAddr, TcpListener, TcpStream};
-use std::os::unix::io::{AsRawFd, RawFd};
+use std::collections::VecDeque;
+use std::io::{Read, Write};
+use std::net::{SocketAddr, TcpListener, TcpStream, ToSocketAddrs};
+use std::os::unix::io::{AsFd, AsRawFd, BorrowedFd, RawFd};
 use std::{io, net};
 
 use crate::actors::IoEv;
@@ -13,8 +14,36 @@ pub enum TcpAction {
 
 pub struct TcpConnection<L: Layout> {
     stream: TcpStream,
-    inbound: bool,
-    controller: Controller<L>,
+    queue: VecDeque<u8>,
+    read_buf: Vec<u8>,
+    pub(super) controller: Controller<L>,
+    pub(super) is_inbound: bool,
+}
+
+impl<L: Layout> TcpConnection<L> {
+    pub fn connect(addr: impl ToSocketAddrs, controller: Controller<L>) -> io::Result<Self> {
+        let read_buf = Vec::with_capacity(u16::MAX as usize);
+
+        Ok(Self {
+            stream: TcpStream::connect(addr)?,
+            queue: empty!(),
+            read_buf,
+            controller,
+            is_inbound: false,
+        })
+    }
+
+    pub fn accept(stream: TcpStream, controller: Controller<L>) -> io::Result<Self> {
+        let read_buf = Vec::with_capacity(u16::MAX as usize);
+
+        Ok(Self {
+            stream,
+            queue: empty!(),
+            read_buf,
+            controller,
+            is_inbound: true,
+        })
+    }
 }
 
 impl<L: Layout> Actor for TcpConnection<L> {
@@ -31,15 +60,10 @@ impl<L: Layout> Actor for TcpConnection<L> {
     where
         Self: Sized,
     {
-        let (inbound, stream) = match context {
-            TcpAction::Accept(stream, _addr) => (true, stream),
-            TcpAction::Connect(addr) => (false, TcpStream::connect(addr)?),
-        };
-        Ok(Self {
-            stream,
-            controller,
-            inbound,
-        })
+        match context {
+            TcpAction::Accept(stream, _addr) => Self::accept(stream, controller),
+            TcpAction::Connect(addr) => Self::connect(addr, controller),
+        }
     }
 
     fn id(&self) -> Self::Id {
@@ -47,12 +71,18 @@ impl<L: Layout> Actor for TcpConnection<L> {
     }
 
     fn io_ready(&mut self, io: IoEv) -> Result<(), Self::Error> {
-        // We do nothing - data must be handler in the upper level resource
+        if io.is_readable {
+            self.flush()?;
+        }
+        if io.is_writable {
+            let len = self.stream.read(&mut self.read_buf)?;
+            self.queue.extend(&self.read_buf[..len]);
+        }
         Ok(())
     }
 
     fn handle_cmd(&mut self, data: Self::Cmd) -> Result<(), Self::Error> {
-        self.stream.write_all(&data)
+        self.write_all(&data)
     }
 
     fn handle_err(&mut self, err: Self::Error) -> Result<(), Self::Error> {
@@ -60,15 +90,37 @@ impl<L: Layout> Actor for TcpConnection<L> {
     }
 }
 
+impl<L: Layout> Read for TcpConnection<L> {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        self.queue.read(buf)
+    }
+}
+
+impl<L: Layout> Write for TcpConnection<L> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.stream.write(buf)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.stream.flush()
+    }
+}
+
+impl<L: Layout> AsRawFd for TcpConnection<L> {
+    fn as_raw_fd(&self) -> RawFd {
+        self.stream.as_raw_fd()
+    }
+}
+
+impl<L: Layout> AsFd for TcpConnection<L> {
+    fn as_fd(&self) -> BorrowedFd<'_> {
+        self.stream.as_fd()
+    }
+}
+
 pub struct TcpSpawner<L: Layout, const SESSION_POOL_ID: u32> {
     socket: TcpListener,
     controller: Controller<L>,
-}
-
-impl<L: Layout, const SESSION_POOL_ID: u32> AsRawFd for TcpSpawner<L, SESSION_POOL_ID> {
-    fn as_raw_fd(&self) -> RawFd {
-        self.socket.as_raw_fd()
-    }
 }
 
 impl<L: Layout, const SESSION_POOL_ID: u32> Actor for TcpSpawner<L, SESSION_POOL_ID> {
@@ -108,5 +160,17 @@ impl<L: Layout, const SESSION_POOL_ID: u32> Actor for TcpSpawner<L, SESSION_POOL
     fn handle_err(&mut self, err: Self::Error) -> Result<(), Self::Error> {
         // Listener does not know how to handle errors, so it just propagates them
         Err(err)
+    }
+}
+
+impl<L: Layout, const SESSION_POOL_ID: u32> AsRawFd for TcpSpawner<L, SESSION_POOL_ID> {
+    fn as_raw_fd(&self) -> RawFd {
+        self.socket.as_raw_fd()
+    }
+}
+
+impl<L: Layout, const SESSION_POOL_ID: u32> AsFd for TcpSpawner<L, SESSION_POOL_ID> {
+    fn as_fd(&self) -> BorrowedFd<'_> {
+        self.socket.as_fd()
     }
 }
