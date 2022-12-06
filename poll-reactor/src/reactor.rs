cd47f23b882295099c85f8b3c33e5853d84623b3
Dr Maxim Orlovsky <orlovsky@cyphernet.org>
2022-12-06 08:56:19 +0100

    Implement poll action handler

--- poll-reactor/src/reactor.rs
+++ poll-reactor/src/reactor.rs
@@ -1,27 +1,38 @@
-use amplify::confinement::Confined;
 use std::collections::HashMap;
-use std::os::unix::io::RawFd;
+use std::io;
+use std::os::unix::io::{AsRawFd, RawFd};
 use std::thread::JoinHandle;
 use std::time::Duration;
 
 use crossbeam_channel as chan;
 
 use crate::poller::Poll;
-use crate::resource::Resource;
+use crate::{Resource, ResourceId};
 
-pub enum Action<L: Resource, C: Resource, const MAX_SIZE: usize> {
-    RegisterListener(L),
-    RegisterConnection(C),
-    UnregisterListener(L),
-    UnregisterConnection(C),
-    Send(C::Id, Confined<Vec<u8>, 1, MAX_SIZE>),
+#[derive(Debug, Display, Error)]
+#[display(doc_comments)]
+pub enum Error<L: ResourceId, T: ResourceId> {
+    /// unknown listener {0}
+    ListenerUnknown(L),
+
+    /// no connection with to peer {0}
+    PeerUnknown(T),
+
+    /// connection with peer {0} got broken
+    PeerDisconnected(T, io::Error),
 }
 
-pub trait Handler<const MAX_DATA_SIZE: usize = { usize::MAX }>:
-    Send + Iterator<Item = Action<Self::Listener, Self::Connection, MAX_DATA_SIZE>>
-{
+pub enum Action<L: Resource, T: Resource> {
+    RegisterListener(L),
+    RegisterTransport(T),
+    UnregisterListener(L::Id),
+    UnregisterTransport(T::Id),
+    Send(T::Id, Vec<T::Message>),
+}
+
+pub trait Handler: Send + Iterator<Item = Action<Self::Listener, Self::Transport>> {
     type Listener: Resource;
-    type Connection: Resource;
+    type Transport: Resource;
     type Command: Send;
 
     fn handle_wakeup(&mut self);
@@ -33,14 +44,19 @@ pub trait Handler<const MAX_DATA_SIZE: usize = { usize::MAX }>:
         duration: Duration,
     );
 
-    fn handle_connection_event(
+    fn handle_transport_event(
         &mut self,
-        id: <Self::Connection as Resource>::Id,
-        event: <Self::Connection as Resource>::Event,
+        id: <Self::Transport as Resource>::Id,
+        event: <Self::Transport as Resource>::Event,
         duration: Duration,
     );
 
     fn handle_command(&mut self, cmd: Self::Command);
+
+    fn handle_error(
+        &mut self,
+        err: Error<<Self::Listener as Resource>::Id, <Self::Transport as Resource>::Id>,
+    );
 }
 
 pub struct Reactor<S: Handler> {
@@ -65,9 +81,9 @@ impl<S: Handler> Reactor<S> {
                 control_recv,
                 shutdown_recv,
                 listeners: empty!(),
-                connections: empty!(),
+                transports: empty!(),
                 listener_map: empty!(),
-                connection_map: empty!(),
+                transport_map: empty!(),
             };
 
             runtime.run();
@@ -87,9 +103,9 @@ pub struct Runtime<H: Handler, P: Poll> {
     control_recv: chan::Receiver<H::Command>,
     shutdown_recv: chan::Receiver<()>,
     listener_map: HashMap<RawFd, <H::Listener as Resource>::Id>,
-    connection_map: HashMap<RawFd, <H::Connection as Resource>::Id>,
+    transport_map: HashMap<RawFd, <H::Transport as Resource>::Id>,
     listeners: HashMap<<H::Listener as Resource>::Id, H::Listener>,
-    connections: HashMap<<H::Connection as Resource>::Id, H::Connection>,
+    transports: HashMap<<H::Transport as Resource>::Id, H::Transport>,
     // waker
     // timeouts
 }
@@ -113,23 +129,71 @@ impl<H: Handler, P: Poll> Runtime<H, P> {
                 for event in res {
                     self.service.handle_listener_event(*id, event, duration);
                 }
-            } else if let Some(id) = self.connection_map.get(&fd) {
-                let res = self.connections.get_mut(id).expect("resource disappeared");
+            } else if let Some(id) = self.transport_map.get(&fd) {
+                let res = self.transports.get_mut(id).expect("resource disappeared");
                 res.handle_io(io);
                 for event in res {
-                    self.service.handle_connection_event(*id, event, duration);
+                    self.service.handle_transport_event(*id, event, duration);
                 }
             }
         }
 
         while let Some(action) = self.service.next() {
-            match action {
-                Action::RegisterListener(_) => {}
-                Action::RegisterConnection(_) => {}
-                Action::UnregisterListener(_) => {}
-                Action::UnregisterConnection(_) => {}
-                Action::Send(id, _) => {}
+            // NB: Deadlock may happen here if the service will generate events over and over
+            // in the handle_* calls we may never get out of this loop
+        }
+    }
+
+    fn handle_action(
+        &mut self,
+        action: Action<H::Listener, H::Transport>,
+    ) -> Result<(), Error<<H::Listener as Resource>::Id, <H::Transport as Resource>::Id>> {
+        match action {
+            Action::RegisterListener(listener) => {
+                let id = listener.id();
+                let fd = listener.as_raw_fd();
+                self.poller.register(fd);
+                self.listeners.insert(id, listener);
+                self.listener_map.insert(fd, id);
+            }
+            Action::RegisterTransport(transport) => {
+                let id = transport.id();
+                let fd = transport.as_raw_fd();
+                self.poller.register(fd);
+                self.transports.insert(id, transport);
+                self.transport_map.insert(fd, id);
+            }
+            Action::UnregisterListener(id) => {
+                let listener = self
+                    .listeners
+                    .remove(&id)
+                    .ok_or(Error::ListenerUnknown(id))?;
+                let fd = listener.as_raw_fd();
+                self.listener_map
+                    .remove(&fd)
+                    .expect("listener index content doesn't match registered listeners");
+                self.poller.unregister(fd);
+            }
+            Action::UnregisterTransport(id) => {
+                let transport = self.transports.remove(&id).ok_or(Error::PeerUnknown(id))?;
+                let fd = transport.as_raw_fd();
+                self.transport_map
+                    .remove(&fd)
+                    .expect("transport index content doesn't match registered transports");
+                self.poller.unregister(fd);
+            }
+            Action::Send(id, msgs) => {
+                let transport = self.transports.get_mut(&id).ok_or(Error::PeerUnknown(id))?;
+                for msg in msgs {
+                    // If we fail on sending any message this means disconnection (I/O write
+                    // has failed for a given transport). We report error -- and lose all other
+                    // messages we planned to send
+                    transport
+                        .send(msg)
+                        .map_err(|err| Error::PeerDisconnected(id, err))?;
+                }
             }
         }
+        Ok(())
     }
 }
