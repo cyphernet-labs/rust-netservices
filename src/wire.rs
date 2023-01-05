de89be9889fc769d750e25861f7b78e16dfb9e1b
Dr Maxim Orlovsky <orlovsky@cyphernet.org>
2023-01-05 17:59:05 +0100

    Make reactor resources state machines

--- src/wire.rs
+++ src/wire.rs
@@ -1,6 +1,5 @@
-use std::collections::VecDeque;
 use std::fmt::Debug;
-use std::io::{Read, Write};
+use std::io::Write;
 use std::net::TcpListener;
 use std::os::unix::io::{AsRawFd, RawFd};
 use std::{io, net};
@@ -8,7 +7,7 @@ use std::{io, net};
 use reactor::poller::IoEv;
 use reactor::Resource;
 
-use crate::{Frame, Marshaller, NetConnection, NetListener, NetSession};
+use crate::{NetConnection, NetListener, NetSession};
 
 /// Socket read buffer size.
 const READ_BUFFER_SIZE: usize = u16::MAX as usize;
@@ -20,14 +19,13 @@ const WRITE_TIMEOUT: std::time::Duration = std::time::Duration::from_secs(3);
 #[derive(Debug)]
 pub enum ListenerEvent<S: NetSession> {
     Accepted(S),
-    Error(io::Error),
+    Failure(io::Error),
 }
 
 #[derive(Debug)]
 pub struct NetAccept<S: NetSession, L: NetListener<Stream = S::Connection> = TcpListener> {
     session_context: S::Context,
     listener: L,
-    events: VecDeque<ListenerEvent<S>>,
 }
 
 impl<L: NetListener<Stream = S::Connection>, S: NetSession> AsRawFd for NetAccept<S, L> {
@@ -53,7 +51,6 @@ impl<L: NetListener<Stream = S::Connection>, S: NetSession> NetAccept<S, L> {
         Ok(Self {
             session_context,
             listener,
-            events: empty!(),
         })
     }
 
@@ -61,46 +58,32 @@ impl<L: NetListener<Stream = S::Connection>, S: NetSession> NetAccept<S, L> {
         self.listener.local_addr()
     }
 
-    fn handle_accept(&mut self) -> io::Result<()> {
+    fn handle_accept(&mut self) -> io::Result<S> {
         let mut stream = self.listener.accept()?;
         stream.set_read_timeout(Some(READ_TIMEOUT))?;
         stream.set_write_timeout(Some(WRITE_TIMEOUT))?;
         stream.set_nonblocking(true)?;
-        let session = S::accept(stream, &self.session_context);
-        self.events.push_back(ListenerEvent::Accepted(session));
-        Ok(())
+        Ok(S::accept(stream, &self.session_context))
     }
 }
 
 impl<L: NetListener<Stream = S::Connection>, S: NetSession> Resource for NetAccept<S, L> {
     type Id = net::SocketAddr;
     type Event = ListenerEvent<S>;
-    type Message = (); // Indicates incoming connection
 
     fn id(&self) -> Self::Id {
         self.listener.local_addr()
     }
 
-    fn handle_io(&mut self, ev: IoEv) -> usize {
+    fn handle_io(&mut self, ev: IoEv) -> Option<Self::Event> {
         if ev.is_writable {
-            if let Err(err) = self.handle_accept() {
-                self.events.push_back(ListenerEvent::Error(err));
-                0
-            } else {
-                1
-            }
+            Some(match self.handle_accept() {
+                Err(err) => ListenerEvent::Failure(err),
+                Ok(session) => ListenerEvent::Accepted(session),
+            })
         } else {
-            0
+            None
         }
-    }
-
-    fn post(&mut self, _msg: Self::Message) -> io::Result<()> {
-        panic!("must not send messages to the network listener")
-    }
-
-    fn flush(&mut self) -> io::Result<()> {
-        // Nothing to do here
-        Ok(())
     }
 
     fn disconnect(self) -> io::Result<()> {
@@ -109,46 +92,45 @@ impl<L: NetListener<Stream = S::Connection>, S: NetSession> Resource for NetAcce
     }
 }
 
-impl<L: NetListener<Stream = S::Connection>, S: NetSession> Iterator for NetAccept<S, L> {
-    type Item = ListenerEvent<S>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.events.pop_front()
-    }
+pub enum SessionEvent<S: NetSession> {
+    Established(S::Id),
+    Data(Vec<u8>),
+    Terminated(io::Error),
 }
 
-pub enum SessionEvent<S: NetSession, F: Frame> {
-    SessionEstablished(S::Id),
-    Message(F),
-    FrameFailure(F::Error),
-    ConnectionFailure(io::Error),
-    Disconnected,
+#[derive(Clone, Copy, Ord, PartialOrd, Eq, PartialEq, Hash, Debug)]
+pub enum TransportState {
+    Handshake,
+    Active,
+    Terminated,
 }
 
-pub struct NetTransport<S: NetSession, F: Frame> {
+pub struct NetTransport<S: NetSession> {
+    state: TransportState,
     session: S,
-    marshaller: Marshaller,
     inbound: bool,
-    events: VecDeque<SessionEvent<S, F>>,
 }
 
-impl<S: NetSession, F: Frame> AsRawFd for NetTransport<S, F> {
+impl<S: NetSession> AsRawFd for NetTransport<S> {
     fn as_raw_fd(&self) -> RawFd {
         self.session.as_raw_fd()
     }
 }
 
-impl<S: NetSession, F: Frame> NetTransport<S, F> {
-    pub fn upgrade(mut session: S, inbound: bool) -> io::Result<Self> {
+impl<S: NetSession> NetTransport<S> {
+    fn upgrade(mut session: S, inbound: bool) -> io::Result<Self> {
         session.set_read_timeout(Some(READ_TIMEOUT))?;
         session.set_write_timeout(Some(WRITE_TIMEOUT))?;
         session.set_nonblocking(true)?;
         Ok(Self {
+            state: TransportState::Handshake,
             session,
-            marshaller: default!(),
             inbound,
-            events: empty!(),
         })
+    }
+
+    pub fn accept(session: S) -> io::Result<Self> {
+        Self::upgrade(session, true)
     }
 
     pub fn connect(addr: S::PeerAddr, context: &S::Context) -> io::Result<Self> {
@@ -156,39 +138,6 @@ impl<S: NetSession, F: Frame> NetTransport<S, F> {
         let mut me = Self::upgrade(session, true)?;
         me.inbound = false;
         Ok(me)
-    }
-
-    /// Downgrades transport into an underlying session object and a buffer
-    /// which contains data which were not consumed in form of messages.
-    ///
-    /// # Errors
-    ///
-    /// If the output queue has unset data fails to downgrade and returns
-    /// unmodified self back
-    pub fn downgrade(mut self) -> Result<(S, Vec<u8>), Self> {
-        let buf = match self.marshaller.drain() {
-            Err(marshaller) => {
-                self.marshaller = marshaller;
-                return Err(self);
-            }
-            Ok(buf) => buf,
-        };
-        let mut drain = Vec::<u8>::with_capacity(buf.len());
-        for ev in self.events {
-            match ev {
-                SessionEvent::Message(frame) => {
-                    frame
-                        .marshall(&mut drain)
-                        .expect("unable to marshall already unmarshalled message");
-                }
-                SessionEvent::FrameFailure(_) => {
-                    // Just ignore this since the message data are in the buffer
-                }
-                _ => {}
-            }
-        }
-        drain.extend(buf);
-        Ok((self.session, drain))
     }
 
     pub fn is_inbound(&self) -> bool {
@@ -199,8 +148,8 @@ impl<S: NetSession, F: Frame> NetTransport<S, F> {
         !self.is_inbound()
     }
 
-    pub fn handshake_completed(&self) -> bool {
-        self.session.handshake_completed()
+    pub fn state(&self) -> TransportState {
+        self.state
     }
 
     pub fn local_addr(&self) -> <S::Connection as NetConnection>::Addr {
@@ -211,24 +160,38 @@ impl<S: NetSession, F: Frame> NetTransport<S, F> {
         self.session.expect_id()
     }
 
-    fn handle_writable(&mut self) {
-        if let Err(err) = self.session.flush() {
-            self.events.push_back(SessionEvent::ConnectionFailure(err));
+    fn handle_writable(&mut self) -> Option<SessionEvent<S>> {
+        debug_assert_ne!(
+            self.state,
+            TransportState::Terminated,
+            "read on terminated transport"
+        );
+        match self.session.flush() {
+            Err(err) => Some(SessionEvent::Terminated(err)),
+            Ok(_) => None,
         }
     }
 
-    fn handle_readable(&mut self) {
+    fn handle_readable(&mut self) -> Option<SessionEvent<S>> {
+        debug_assert_ne!(
+            self.state,
+            TransportState::Terminated,
+            "read on terminated transport"
+        );
+
         // We need to save the state before doing the read below
-        let was_established = self.handshake_completed();
-        let mut buffer = [0; READ_BUFFER_SIZE];
-        let res = self.session.read(&mut buffer);
-        if !was_established && self.handshake_completed() {
-            self.events
-                .push_back(SessionEvent::SessionEstablished(self.session.expect_id()));
-        }
+        let was_established = self.state == TransportState::Handshake;
+        let mut buffer = Vec::with_capacity(READ_BUFFER_SIZE);
+        let res = self.session.read_to_end(&mut buffer);
         match res {
             Ok(0) if !was_established => {
-                // Do nothing since we haven't established session yet
+                if self.session.handshake_completed() {
+                    self.state = TransportState::Active;
+                    return Some(SessionEvent::Established(self.session.expect_id()));
+                } else {
+                    // Do nothing since we haven't established session yet
+                    None
+                }
             }
             // Nb. Since `poll`, which this reactor is based on, is *level-triggered*,
             // we will be notified again if there is still data to be read on the socket.
@@ -237,68 +200,42 @@ impl<S: NetSession, F: Frame> NetTransport<S, F> {
             Ok(0) => {
                 // If we get zero bytes read as a return value, it means the peer has
                 // performed an orderly shutdown.
-                self.events.push_back(SessionEvent::Disconnected)
+                self.state = TransportState::Terminated;
+                Some(SessionEvent::Terminated(io::ErrorKind::Interrupted.into()))
             }
             Ok(len) => {
-                self.marshaller
-                    .write_all(&buffer[..len])
-                    .expect("in-memory writer");
-                loop {
-                    match self.marshaller.pop() {
-                        Ok(Some(msg)) => self.events.push_back(SessionEvent::Message(msg)),
-                        Ok(None) => break,
-                        Err(err) => {
-                            self.events.push_back(SessionEvent::FrameFailure(err));
-                            break;
-                        }
-                    }
-                }
+                debug_assert!(was_established);
+                debug_assert_eq!(len, buffer.len());
+                Some(SessionEvent::Data(buffer))
             }
-            Err(err) if err.kind() == io::ErrorKind::WouldBlock => {
-                // This shouldn't normally happen, since this function is only called
-                // when there's data on the socket. We leave it here in case external
-                // conditions change.
-                unreachable!()
+            Err(err) if err.kind() == io::ErrorKind::WouldBlock => Some(SessionEvent::Data(buffer)),
+            Err(err) => {
+                self.state = TransportState::Terminated;
+                Some(SessionEvent::Terminated(err))
             }
-            Err(err) => self.events.push_back(SessionEvent::ConnectionFailure(err)),
         }
     }
 }
 
-impl<S: NetSession, F: Frame> Resource for NetTransport<S, F>
+impl<S: NetSession> Resource for NetTransport<S>
 where
     S::TransitionAddr: Into<net::SocketAddr>,
 {
     type Id = RawFd;
-    type Event = SessionEvent<S, F>;
-    type Message = F;
+    type Event = SessionEvent<S>;
 
     fn id(&self) -> Self::Id {
         self.session.as_raw_fd()
     }
 
-    fn handle_io(&mut self, ev: IoEv) -> usize {
-        let len = self.events.len();
+    fn handle_io(&mut self, ev: IoEv) -> Option<Self::Event> {
         if ev.is_writable {
-            self.handle_writable();
+            self.handle_writable()
         } else if ev.is_readable {
-            self.handle_readable();
+            self.handle_readable()
+        } else {
+            unreachable!()
         }
-        // TODO: Handle exceptions like hangouts etc
-        self.events.len() - len
-    }
-
-    fn post(&mut self, msg: Self::Message) -> io::Result<()> {
-        self.marshaller.push(msg);
-        let mut buf = vec![0u8; self.marshaller.queue_len()];
-        self.marshaller.read_exact(&mut buf).expect(
-            "queue length reported by framer doesn't exceeds the length of the returned data",
-        );
-        self.session.write_all(&buf)
-    }
-
-    fn flush(&mut self) -> io::Result<()> {
-        self.session.flush()
     }
 
     fn disconnect(self) -> io::Result<()> {
@@ -306,10 +243,15 @@ where
     }
 }
 
-impl<S: NetSession, F: Frame> Iterator for NetTransport<S, F> {
-    type Item = SessionEvent<S, F>;
+impl<S: NetSession> Write for NetTransport<S>
+where
+    S::TransitionAddr: Into<net::SocketAddr>,
+{
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.session.write(&buf)
+    }
 
-    fn next(&mut self) -> Option<Self::Item> {
-        self.events.pop_front()
+    fn flush(&mut self) -> io::Result<()> {
+        self.session.flush()
     }
 }
