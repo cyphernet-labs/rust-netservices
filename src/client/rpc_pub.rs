c3fdda14838ee0e77fb86ca9d34fa344b0e6106f
Dr Maxim Orlovsky <orlovsky@cyphernet.io>
2024-07-20 10:29:29 +0200

    client: parametrize MsgPub

--- src/client/rpc_pub.rs
+++ src/client/rpc_pub.rs
@@ -22,13 +22,12 @@
 use std::any::Any;
 use std::collections::HashMap;
 use std::io;
-use std::io::Error;
 use std::marker::PhantomData;
 
 use super::{Client, ClientDelegate, OnDisconnect};
 use crate::{ImpossibleResource, NetSession, NetTransport};
 
-pub type Cb = fn(Vec<u8>);
+pub type Cb<Rep> = fn(Rep);
 
 pub const CLIENT_MSG_ID_RPC: u8 = 0x01u8;
 pub const CLIENT_MSG_ID_PUB: u8 = 0x10u8;
@@ -106,17 +105,53 @@ impl From<Msg> for Vec<u8> {
     }
 }
 
-pub trait RpcPubDelegate<A: Send, S: NetSession>: ClientDelegate<A, S, Cb, Reply = Msg> {
-    fn on_message_pub(&self, id: u16, data: Vec<u8>);
-    // TODO: Make it just an error
-    fn on_reply_unrecognized(&self, id: u64, data: Vec<u8>);
-    fn on_reply_timeout(&self, id: u64);
+#[derive(Clone, Eq, PartialEq, Hash, Debug, Display, Error)]
+#[display(doc_comments)]
+pub enum MsgError {
+    UnparsableMsg(String),
+    UnparsableReply(u64, String),
+    UnparsablePub(String),
+    MismatchingReply(u64, Vec<u8>),
+}
+
+pub trait RpcPubDelegate<A: Send, S: NetSession>: Send {
+    /// The reply type which must be parsable from a byte blob.
+    type Reply: TryFrom<Vec<u8>, Error: std::error::Error>;
+    type PubMsg: TryFrom<Vec<u8>, Error: std::error::Error>;
+
+    /// Asks the delegate to construct a connection to the remote server and return it as a form of
+    /// [`NetSession`] to be registered and managed by the reactor and [`ClientService`] inside of
+    /// it.
+    fn connect(&self, remote: &A) -> S;
+
+    /// Notifies about the successful establishment of the session with the server. The `attempt`
+    /// argument specifies the number of the connection attempt which has succeeded, if a
+    /// reconnection or failed connection had happened.
+    fn on_established(&self, artifact: S::Artifact, attempt: usize);
+
+    /// Notifies about failed connection to the server. As a response, the client business logic can
+    /// ask to re-establish connection by returning [`OnDisconnect::Reconnect`]. Otherwise, the
+    /// reactor will terminate.
+    fn on_disconnect(&self, err: io::Error, attempt: usize) -> OnDisconnect;
+
+    /// Callback for processing reactor [`reactor::Error`]s.
+    fn on_io_error(&self, err: reactor::Error<ImpossibleResource, NetTransport<S>>);
+
+    /// Callback for processing invalid message received from the server which can't be parsed into
+    /// [`Self::Reply`] type.
+    fn on_msg_error(&self, err: MsgError);
+
+    fn on_msg_pub(&self, id: u16, msg: Self::PubMsg);
+
+    fn on_reply_timed_out(&self, id: u64);
+    /// Callback for processing the message received from the server.
+    fn on_reply(&mut self, reply: Self::Reply);
 }
 
 pub struct RpcPubService<A: Send, S: NetSession, D: RpcPubDelegate<A, S>> {
     delegate: D,
     last_id: u64,
-    callbacks: HashMap<u64, Cb>,
+    callbacks: HashMap<u64, Cb<D::Reply>>,
     _phantom: PhantomData<(A, S)>,
 }
 
@@ -131,7 +166,7 @@ impl<A: Send, S: NetSession, D: RpcPubDelegate<A, S>> RpcPubService<A, S, D> {
     }
 }
 
-impl<A: Send, S: NetSession, D: RpcPubDelegate<A, S>> ClientDelegate<A, S, Cb>
+impl<A: Send, S: NetSession, D: RpcPubDelegate<A, S>> ClientDelegate<A, S, Cb<D::Reply>>
     for RpcPubService<A, S, D>
 {
     type Reply = Msg;
@@ -142,7 +177,7 @@ impl<A: Send, S: NetSession, D: RpcPubDelegate<A, S>> ClientDelegate<A, S, Cb>
         self.delegate.on_established(artifact, attempt)
     }
 
-    fn on_disconnect(&self, err: Error, attempt: usize) -> OnDisconnect {
+    fn on_disconnect(&self, err: io::Error, attempt: usize) -> OnDisconnect {
         self.delegate.on_disconnect(err, attempt)
     }
 
@@ -150,22 +185,32 @@ impl<A: Send, S: NetSession, D: RpcPubDelegate<A, S>> ClientDelegate<A, S, Cb>
         match msg.id {
             RpcPubId::Rpc(id) => {
                 if let Some(cb) = self.callbacks.remove(&id) {
-                    cb(msg.payload)
+                    match D::Reply::try_from(msg.payload) {
+                        Ok(reply) => cb(reply),
+                        Err(e) => {
+                            self.delegate.on_msg_error(MsgError::UnparsableReply(id, e.to_string()))
+                        }
+                    }
                 } else {
-                    self.delegate.on_reply_unrecognized(id, msg.payload);
+                    self.delegate.on_msg_error(MsgError::MismatchingReply(id, msg.payload));
                 }
             }
-            RpcPubId::Pub(id) => self.delegate.on_message_pub(id, msg.payload),
+            RpcPubId::Pub(id) => match D::PubMsg::try_from(msg.payload) {
+                Ok(msg_pub) => self.delegate.on_msg_pub(id, msg_pub),
+                Err(e) => self.delegate.on_msg_error(MsgError::UnparsablePub(e.to_string())),
+            },
         }
     }
 
-    fn on_reply_unparsable(&self, err: ParseMsgErr) { self.delegate.on_reply_unparsable(err) }
-
-    fn on_error(&self, err: reactor::Error<ImpossibleResource, NetTransport<S>>) {
-        self.delegate.on_error(err)
+    fn on_reply_unparsable(&self, err: <Self::Reply as TryFrom<Vec<u8>>>::Error) {
+        self.delegate.on_msg_error(MsgError::UnparsableMsg(err.to_string()))
     }
 
-    fn before_send(&mut self, data: Vec<u8>, cb: Cb) -> Vec<u8> {
+    fn on_error(&self, err: reactor::Error<ImpossibleResource, NetTransport<S>>) {
+        self.delegate.on_io_error(err)
+    }
+
+    fn before_send(&mut self, data: Vec<u8>, cb: Cb<D::Reply>) -> Vec<u8> {
         let id = self.last_id;
         let mut req = Vec::with_capacity(data.len() + 9);
         let check = self.callbacks.insert(id, cb);
@@ -180,21 +225,27 @@ impl<A: Send, S: NetSession, D: RpcPubDelegate<A, S>> ClientDelegate<A, S, Cb>
 
 /// The client runtime containing reactor thread managing connection to the remote server and the
 /// use of the server APIs.
-pub struct RpcPubClient<Req: Into<Vec<u8>>> {
-    inner: Client<Req, Cb>,
+pub struct RpcPubClient<Req: Into<Vec<u8>>, Rep: TryFrom<Vec<u8>> + 'static> {
+    inner: Client<Req, Cb<Rep>>,
 }
 
-impl<Req: Into<Vec<u8>>> RpcPubClient<Req> {
-    pub fn new<A: Send + 'static, S: NetSession + 'static, D: RpcPubDelegate<A, S> + 'static>(
+impl<Req: Into<Vec<u8>>, Rep: TryFrom<Vec<u8>> + 'static> RpcPubClient<Req, Rep> {
+    pub fn new<
+        A: Send + 'static,
+        S: NetSession + 'static,
+        D: RpcPubDelegate<A, S, Reply = Rep> + 'static,
+    >(
         delegate: D,
         remote: A,
     ) -> io::Result<Self> {
         let rpc_pub_service = RpcPubService::new(delegate);
-        let client = Client::new(rpc_pub_service, remote)?;
+        let client = Client::<Req, Cb<Rep>>::new(rpc_pub_service, remote)?;
         Ok(Self { inner: client })
     }
 
-    pub fn send(&mut self, data: Req, cb: Cb) -> io::Result<()> { self.inner.send_extra(data, cb) }
+    pub fn send(&mut self, data: Req, cb: Cb<Rep>) -> io::Result<()> {
+        self.inner.send_extra(data, cb)
+    }
 
     pub fn terminate(self) -> Result<(), Box<dyn Any + Send>> { self.inner.terminate() }
 }
